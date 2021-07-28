00d949279d9f77153217d23a74e71c55a2c50c64
Jayashree-D <jayashree-d@hcl.com>
2021-07-28 18:17:03 +0530

    Power and Health status LED

---
+++ fault-monitor/multi-purpose-status.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "../utils.hpp"
+
+#include <nlohmann/json.hpp>
+#include <phosphor-logging/elog.hpp>
+
+#include <algorithm>
+#include <fstream>
+
+namespace phosphor
+{
+namespace led
+{
+namespace multi
+{
+namespace purpose
+{
+namespace status
+{
+
+using namespace phosphor::led::utils;
+using namespace phosphor::logging;
+
+class Status
+{
+  public:
+    Status(sdbusplus::bus::bus& bus) : bus(bus)
+    {
+        if (loadConfigValues() == "DebugCard")
+        {
+            matchSignal = std::make_unique<sdbusplus::bus::match_t>(
+                bus,
+                sdbusplus::bus::match::rules::type::signal() +
+                    sdbusplus::bus::match::rules::member("PropertiesChanged") +
+                    sdbusplus::bus::match::rules::path(
+                        "/xyz/openbmc_project/Chassis/Buttons/Selector0") +
+                    sdbusplus::bus::match::rules::interface(
+                        "org.freedesktop.DBus.Properties"),
+                [this](sdbusplus::message::message& msg) {
+                    std::string objectName;
+                    std::map<std::string, std::variant<uint16_t>> msgData;
+                    msg.read(objectName, msgData);
+
+                    auto valPropMap = msgData.find("Position");
+                    {
+                        if (valPropMap != msgData.end())
+                        {
+                            DebugCard();
+                        }
+                    }
+                });
+        }
+        else
+        {
+            sled();
+        }
+    }
+
+  private:
+    /* @brief sdbusplus D-Bus connection. */
+    sdbusplus::bus::bus& bus;
+
+    /** @brief sdbusplus signal matches for Monitor */
+    std::unique_ptr<sdbusplus::bus::match_t> matchSignal;
+
+    DBusHandler dBusHandler;
+
+    void sled();
+
+    void DebugCard();
+
+    const PropertyValue getPropertyValue(const std::string& objectPath,
+                                         const std::string& interface,
+                                         const std::string& propertyName);
+
+    void setLedGroup(const std::string& objectPath, bool value);
+
+    void setPhysicalLed(const std::string& objectPath,
+                        const std::string& action, uint8_t dutyOn,
+                        uint16_t period);
+
+    void selectLedGroup(uint16_t position, const std::string& powerStatus,
+                        const std::string& healthStatus);
+
+    std::string loadConfigValues();
+};
+
+} // namespace status
+} // namespace purpose
+} // namespace multi
+} // namespace led
+} // namespace phosphor
